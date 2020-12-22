56aefa0d95a99545b506bab368b732e9b7a51c6a
Alex Ho <alexanderkho92@gmail.com>
2020-12-21 20:28:54 -0700

    Add checkForFilledLines method with pseudocode for clearing lines.

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -10,6 +10,37 @@ import "./Board.css";
 const Board: React.FC = () => {
   const [board, setBoard] = React.useState(initBoard());
 
+  const checkForFilledLines = () => {
+    const rows = new Array(board.length).fill(true);
+    const cols = new Array(board.length).fill(true);
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board.length; j++) {
+        const piece = board[i][j];
+        if (!piece.parentPiece) {
+          rows[i] = false;
+          cols[j] = false;
+        }
+      }
+    }
+    rows.forEach((rowFilled, i) => {
+      if (rowFilled) {
+        //TODO
+        console.log("clearing row ", i);
+      }
+    });
+    cols.forEach((colFilled, j) => {
+      if (colFilled) {
+        //TODO
+        console.log("clearing col ", j);
+      }
+    });
+  };
+
+  React.useEffect(() => {
+    console.log("checking for filled lines...");
+    checkForFilledLines();
+  }, [board]);
+
   const updateBoard = (pieceName: T_Piece, pos: T_Pos) => {
     setBoard((prev) => {
       const [updateRow, updateCol] = pos;
@@ -61,7 +92,7 @@ const Board: React.FC = () => {
       {board.map((row, i) => (
         <div className="row" key={i}>
           {row.map((cell, j) => (
-            <Cell onClick={() => addPiece("RLeft", cell.pos)} key={j}>
+            <Cell onClick={() => addPiece("Square", cell.pos)} key={j}>
               {cell.parentPiece ? <Block piece={cell.parentPiece} /> : null}
             </Cell>
           ))}
